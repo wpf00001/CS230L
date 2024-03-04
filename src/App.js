102e536eef15365948b8d134a821f645d167a979
wpf00001 <wpf00001@mix.wvu.edu>
2024-03-03 23:18:43 -0500

    finalized for week8 labs

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ function App() {
       <h2>Section - 01</h2>
       <p>WVU ID: 800425025</p>
       <p>Hi I am Wesley Forest</p>
-        <div className="card-container">
-          <Card title = "Dogs" content = "Dogs are domestic mammals in the canine family "> </Card>
-
-          <Card title = "Cats" content = "Cats are domestic mammals in the feline family"> </Card>
-
-          <Card title = "Rats" content = "Rats are mammals in the rodent family"> </Card>
-        </div>
+      <Card />
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,28 @@
-function Card({ title, content}) {
-    return (
-        <div className="card" style={{ width: '18rem' }}>
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{content}</p>
+function Card() {
+  return (
+    <div className="card-container">
+        <div className="card1" style={{ width: '18rem', border: '1px solid'}}>
+            <div className="card-body">
+            <h5 className="card-title">Dogs</h5>
+            <p className="card-text">Dogs are domestic canines that are kept as pets.</p>
+            </div>
+      </div>
+
+      <div className="card2" style={{ width: '18rem', border: '1px solid'}}>
+        <div className="card-body">
+          <h5 className="card-title">Cats</h5>
+          <p className="card-text">Cats are domestic Felines that are kept as pets.</p>
+        </div>
+      </div>
+
+      <div className="card3" style={{ width: '18rem', border: '1px solid'}}>
+        <div className="card-body">
+          <h5 className="card-title">Rats</h5>
+          <p className="card-text">Rats are domestic Rodents that are kept as pets.</p>
+        </div>
       </div>
     </div>
-    );
-  }
-  
-  export default Card;
+  );
+}
+
+export default Card;
