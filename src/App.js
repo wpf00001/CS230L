826cd718fc4464eb47a71a11febad569d92184de
wpf00001 <wpf00001@mix.wvu.edu>
2024-03-03 21:33:27 -0500

    import logo from './logo.svg'; import './App.css'; import './Navbar.js'; import './Card.js'; function App() {   return (     <div className="App">       <h1>CS 230L</h1>       <h2>Section - 01</h2>       <p>WVU ID: 800425025</p>       <p>Hi I am </p>
        </div>
      );
    }
    
    export default App;

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './Navbar.js';
+import './Card.js';
 function App() {
   return (
     <div className="App">
       <h1>CS 230L</h1>
       <h2>Section - 01</h2>
       <p>WVU ID: 800425025</p>
-      <p>Hi I am Wesley</p>
-      
+      <p>Hi I am </p>
+      {createCard()}
     </div>
   );
 }
