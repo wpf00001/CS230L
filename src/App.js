a7583af8096f8604673e6e10f4afbf6e1a2cb8f2
wpf00001 <wpf00001@mix.wvu.edu>
2024-03-03 22:56:28 -0500

    new items for week8 lab#

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import './Navbar.js';
-import './Card.js';
+import Navbar from './Navbar';
+import Card from './Card';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 01</h2>
       <p>WVU ID: 800425025</p>
-      <p>Hi I am </p>
-      {createCard()}
+      <p>Hi I am Wesley Forest</p>
+        <div className="card-container">
+          <Card title = "Dogs" content = "Dogs are domestic mammals in the canine family "> </Card>
+
+          <Card title = "Cats" content = "Cats are domestic mammals in the feline family"> </Card>
+
+          <Card title = "Rats" content = "Rats are mammals in the rodent family"> </Card>
+        </div>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,25 @@
-function createNavbar() {
-    // Create the navbar HTML structure
-    const navbarHTML = `
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Your Brand</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    `;
-  
-    // Insert the navbar HTML into the container
-    document.getElementById('navbarContainer').innerHTML = navbarHTML;
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navigation</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <button className="btn btn-link nav-link" type="button">button1</button>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-link nav-link" type="button">button2</button>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-link nav-link" type="button">button3</button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
   }
   
-  // Export the function to make it accessible from other files
-  export { createNavbar };
+  export default Navbar;
